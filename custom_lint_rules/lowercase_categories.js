7b70b095aecf8e8cd652e4be81d6267dbafc4d93
Alec Johnson <alecjohnson55@gmail.com>
2021-04-08 21:14:01 -0700

    add custom categories linter

---
+++ custom_lint_rules/lowercase_categories.js
@@ -0,0 +1,48 @@
+const {
+  forEachLine,
+  getLineMetadata,
+  inlineCommentRe,
+} = require("markdownlint-rule-helpers");
+
+const isLowerCase = (string) => {
+  return string != string.toUpperCase() && string == string.toLowerCase();
+};
+
+const lowercaseCategories = (params, onError) => {
+  const categories_lines = params.frontMatterLines.filter((line) =>
+    line.startsWith("categories: ")
+  );
+  if (categories_lines.length < 1) {
+    onError({
+      lineNumber: 1,
+      detail: "all posts should have categories specified",
+    });
+  } else if (categories_lines.length > 1) {
+    onError({
+      lineNumber: 1,
+      detail: "posts are not allowed to specify multiple lists of categories",
+    });
+  } else {
+    let array_start = categories_lines[0].indexOf("[") + 1;
+    let array_end = categories_lines[0].length - 1;
+    let categories_string = categories_lines[0].substring(
+      array_start,
+      array_end
+    );
+    let categories = categories_string.split(", ");
+    categories.forEach((category) => {
+      if (!isLowerCase(category)) {
+        onError({
+          lineNumber: 1,
+        });
+      }
+    });
+  }
+};
+
+module.exports = {
+  names: ["CMD-002", "all-categories-lowercase"],
+  description: "all categories in frontmatter should be lowercase",
+  tags: ["test"],
+  function: lowercaseCategories,
+};
