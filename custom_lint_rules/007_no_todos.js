7e7d8f7963ad5beab0c334df06a1197a8490c293
Alec Johnson <alecjohnson55@gmail.com>
2021-04-24 10:03:58 -0700

    add linter for no todos in posts

---
+++ custom_lint_rules/007_no_todos.js
@@ -0,0 +1,23 @@
+const {
+  forEachLine,
+  getLineMetadata,
+  inlineCommentRe,
+} = require("markdownlint-rule-helpers");
+
+const noTodos = (params, onError) => {
+  forEachLine(getLineMetadata(params), (line, lineIndex) => {
+    console.log(line);
+    if (line && line.includes("@todo")) {
+      onError({
+        lineNumber: lineIndex
+      })
+    }
+  });
+};
+
+module.exports = {
+  names: ["CMD007", "no-todos"],
+  description: "no todos should be left in the post when it gets published",
+  tags: ["comments"],
+  function: noTodos,
+};
